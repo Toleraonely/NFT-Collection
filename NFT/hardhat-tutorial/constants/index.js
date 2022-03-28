d21eef18fa34aae1d239e8705deec7cfc00d15c2
Toleraonely <295169157@qq.com>
2022-03-28 22:10:48 +0800

    NFT-Collection

---
+++ NFT/hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xf592dA35eF8eF914164f74E3E055d430710b5559";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
